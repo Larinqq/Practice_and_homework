b2aec19e2ebd7655a6391f2d1c968e96f65847b1
Nikita Larkov <larkovns@gmail.com>
2024-03-31 20:12:06 +0300

    refactor code in lesson 6_3

--- Lesson 6_3/tests/test_lesson_6_3.py
+++ Lesson 6_3/tests/test_lesson_6_3.py
@@ -1,39 +1,35 @@
 import sys
 sys.path.append("/home/larinqq/Practice_and_homework/Lesson 6_3/")
-
 from Validators_lesson_6_3 import check_name, check_pass, check_mail, check_pin
 
 
 def test_check_name_func():
-    assert True == check_name('Даниил')
-    assert False == check_name('Р_и_т_а')
-    assert False == check_name('К0нстантин')
-    assert True == check_name('А Ф')
-    assert True == check_name('Елена')
+    assert check_name('Даниил')
+    assert not check_name('Р_и_т_а')
+    assert not check_name('К0нстантин')
+    assert check_name('А Ф')
+    assert check_name('Елена')
 
 
 def test_check_pass_func():
-    assert True == check_pass('secretd00r')
-    assert True == check_pass('huskyeye5')
-    assert False == check_pass('secret')
-    assert True == check_pass('m3wm3wm3w')
-    assert False == check_pass('fh43j_!')
+    assert check_pass('secretd00r')
+    assert check_pass('huskyeye5')
+    assert not check_pass('secret')
+    assert check_pass('m3wm3wm3w')
+    assert not check_pass('fh43j_!')
 
 
 def test_check_mail_func():
-    assert True == check_mail('local@gmail.com')
-    assert False == check_mail('you(at)gmail.com')
-    assert True == check_mail('me@gmail.ru')
-    assert False== check_mail('@abxaz')
-    assert False == check_mail('alarm@.mail.com')
+    assert check_mail('local@gmail.com')
+    assert not check_mail('you(at)gmail.com')
+    assert check_mail('me@gmail.ru')
+    assert not check_mail('@abxaz')
+    assert not check_mail('alarm@.mail.com')
 
 
 def test_check_pin_func():
-    assert True == check_pin('1239')
-    assert False == check_pin('3333')
-    assert False == check_pin('1234')
-    assert False == check_pin('00011')
-    assert True == check_pin('8765')
-
-
-
+    assert check_pin('1239')
+    assert not check_pin('3333')
+    assert not check_pin('1234')
+    assert not check_pin('00011')
+    assert check_pin('8765')
