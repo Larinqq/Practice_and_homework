f6a909598bc972c02bae73837d8b95aaaf3b0c21
Nikita Larkov <larkovns@gmail.com>
2024-04-16 22:39:14 +0300

    prepared project structure and data file(questions)

---
+++ Python basics/Lesson 7/config.py
@@ -0,0 +1,2 @@
+QUESTIONS_FILE_PATH = "/home/larinqq/Practice_and_homework/Python basics/Lesson 7/data/questions.json"
+STAT_FILE_PATH = "/home/larinqq/Practice_and_homework/Python basics/Lesson 7/data/stats.json"
