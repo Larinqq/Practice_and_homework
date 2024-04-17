6f4b694ebfb960c87409a11068ece04f584ba35e
Nikita Larkov <larkovns@gmail.com>
2024-04-17 19:15:48 +0300

    wrote def draw_gamescreen

--- Python basics/Lesson 7/utils.py
+++ Python basics/Lesson 7/utils.py
@@ -1,4 +1,5 @@
 import json
+from config import MAX_CATEGORY_TITTLE_LEN, MAX_CATEGORY_POINTS_LEN, SPACES_BETWEEN_POINTS
 
 
 def questions_load(question_path):
@@ -12,3 +13,24 @@ def questions_load(question_path):
         questions = json.loads(raw_json)
 
     return questions
+
+
+def draw_gamescreen(questions):
+    """
+    Draws gamescreen based on given question. Divides data on categories and point
+    :param questions: dict
+    :return: pretty string
+    """
+    game_field = ''
+    for category, category_data in questions.items():
+        game_field += f"{str(category).ljust(MAX_CATEGORY_TITTLE_LEN)}"
+
+        for category_price, category_question in category_data.items():
+            if category_question["asked"] == "False":
+                game_field += f"{str(category_price).ljust(MAX_CATEGORY_POINTS_LEN+SPACES_BETWEEN_POINTS)}"
+            else:
+                game_field += f"{str('').ljust(MAX_CATEGORY_POINTS_LEN + SPACES_BETWEEN_POINTS)}"
+
+        game_field += "\n"
+
+    return game_field
