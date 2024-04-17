7f31d2933cdcda3a6118b917bd34cda972fd7dab
Nikita Larkov <larkovns@gmail.com>
2024-04-17 19:16:42 +0300

    add max len parametres for tittle, points and spaces

--- Python basics/Lesson 7/config.py
+++ Python basics/Lesson 7/config.py
@@ -1,2 +1,5 @@
 QUESTIONS_FILE_PATH = "/home/larinqq/Practice_and_homework/Python basics/Lesson 7/data/questions.json"
 STAT_FILE_PATH = "/home/larinqq/Practice_and_homework/Python basics/Lesson 7/data/stats.json"
+MAX_CATEGORY_TITTLE_LEN = 15
+MAX_CATEGORY_POINTS_LEN = 3
+SPACES_BETWEEN_POINTS = 3
