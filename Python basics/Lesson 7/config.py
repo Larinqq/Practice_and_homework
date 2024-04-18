95480ac98e71477ee2a1ed2188332044c6283ed2
Nikita Larkov <larkovns@gmail.com>
2024-04-19 00:30:44 +0300

    Added texts to config file

--- Python basics/Lesson 7/config.py
+++ Python basics/Lesson 7/config.py
@@ -3,3 +3,17 @@ STAT_FILE_PATH = "/home/larinqq/Practice_and_homework/Python basics/Lesson 7/dat
 MAX_CATEGORY_TITTLE_LEN = 15
 MAX_CATEGORY_POINTS_LEN = 3
 SPACES_BETWEEN_POINTS = 3
+TOTAL_QUESTIONS = 9
+GREETING_MESSAGE = "Please choose category and price:\n"
+WRONG_INPUT_MESSAGE = "No such category or question"
+CONTINUE_MESSAGE = "Please press ENTER to continue\n"
+RIGHT_ANSWER_MESSAGE = "You're right, points: +"
+WRONG_ANSWER_MESSAGE = "No, you're wrong, points: -"
+REAL_ANSWER_MESSAGE = "Right answer was: "
+QUESTION_MESSAGE = "Translate to russian word: "
+CURRENT_SCORE_MESSAGE = "Your score: "
+FINISH_MESSAGE = "That's all! Thank you for playing\n"
+SCORE_MESSAGE = "Earned points: "
+CORRECT_ANSWER_COUNTER_MESSAGE = "You gave correct answers: "
+INCORRECT_ANSWER_COUNTER_MESSAGE = "You gave incorrect answers: "
+
